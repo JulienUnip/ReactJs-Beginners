6de8a0f3c9257cf773131d6f67a778f560a38876
julien93140 <julien.moratalla@gmail.com>
2020-03-22 21:14:19 +0100

    Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+class Parent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        dataFromChild: '',
+        childName: "Julien Moratalla",
+        childs: [
+          {firstname: "Julien", lastname: "Moratalla"},
+          {firstname: "Julien", lastname: "Aoratalla"},
+        ],
+        fakeDatas: []
+      };
+      this.outputEvent = this.outputEvent.bind(this);
+    };
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({fakeDatas: json});
+      });
+    }
+
+    outputEvent(event) {
+      this.setState({ dataFromChild: 'Event emitted form child' });
+    }
+
+    childNameChange(event) {
+      this.setState({childName: event.target.value});
+    }
+
+    render() {
+      return (
+        <div>
+          <p>Parent</p>
+          <p>Change prefer child name :</p>
+          <input value={this.state.childName} onChange={(e) => {this.childNameChange(e)}} />
+          <Child fakeDatas={this.state.fakeDatas} clickHandler={this.outputEvent} name={this.state.childName} childs={this.state.childs} />
+          <p>Data from child : {this.state.dataFromChild}</p>
+        </div>
+      )
+    }
+};
+
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Child props", props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>My prefered child name is : {this.props.name}</p>
+        <p>Others childs I'm not prefer :</p>
+        <ul className="childs">
+          {this.props.childs
+          .sort((a, b) => a.lastname > b.lastname)
+          .map((item, index) => (
+            <li key={index}>{item.firstname} {item.lastname}</li>
+          ))}
+          <button onClick={this.props.clickHandler}>Pass Datas To Parent</button>
+        </ul>
+      </div>
+    )
+  }
+};
+
 function App() {
+  const styleHeader = {
+    backgroundColor: 'red',
+    width: '100%',
+    height: '100px',
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header style={styleHeader}></header>
+      <p>React app</p>
+      <Parent />
     </div>
   );
 }
